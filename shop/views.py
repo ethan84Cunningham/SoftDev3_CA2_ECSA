e28aed56740e31d3123ef510c681ace9bd98744a
Ethan Cunningham <x00177005@mytudublin.ie>
2021-12-14 18:54:51 +0000

    CA2 P3

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category,Film
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+class FilmCat(ListView):
+    model = Film
+
+    def get(self,request, category_id=None):
+        category = None 
+        films = None
+        if category_id != None:
+            category = get_object_or_404(Category, id = category_id)
+            films = Film.objects.filter(category = category, available = True)
+        else:
+            films = Film.objects.all().filter(available=True)
+
+        return render(request,"shop/category.html",{'category':category, 'products':films})
+
+class FilmDetail(DetailView):
+    model = Film
+
+    def get(self, request, category_id, film_id):
+        try:
+            film = Film.objects.get(category_id = category_id, id = film_id)
+        except Exception as e:
+            raise e
+        return render(request, "shop/film.html", {'film':film})
